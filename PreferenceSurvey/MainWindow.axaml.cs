598120de0e9042e1e9b146238db8b103e881a2bc
Kuba <skibidiziemniak@skebede.com>
2025-03-18 16:03:06 +0100

    start implementing functions

--- PreferenceSurvey/MainWindow.axaml.cs
+++ PreferenceSurvey/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace PreferenceSurvey;
 
@@ -7,5 +8,11 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+    }
+
+    public void FirstSubmit(object? sender, RoutedEventArgs e)
+    {
+        UsernameSubmit.Text = $"Nazwa użytkownika: {Username.Text}";
     }
 }
